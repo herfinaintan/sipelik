8fc4a1d25ecf618545002511d9c2b445e7baaf30
herfinaintan <101050306+herfinaintan@users.noreply.github.com>
2022-07-19 14:01:57 +0700

    Delete delete_kosan.php

--- Admin/delete_kosan.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "header.php";
-	$id = $_GET['id'];
-	$query=mysqli_query($con,"DELETE FROM data_kosan WHERE id_kosan ='$id'");
-	if($query){
-		header('location:datakosan.php');
-	}
-?>
