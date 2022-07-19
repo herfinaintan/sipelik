cf403c661c907f54c2fb94c39fe8973fe5effa0e
herfinaintan <101050306+herfinaintan@users.noreply.github.com>
2022-07-19 14:11:00 +0700

    Add files via upload

---
+++ Assets/js/admin.js
@@ -0,0 +1,9 @@
+/*
+$(window).bind("load resize",function(){
+	if(($this).width > 768){
+		$("sidebar-collapse").addClass("collapse");
+	}else{
+		$("sidebar-collapse").removeClass("collapse");
+	}
+})
+*/
